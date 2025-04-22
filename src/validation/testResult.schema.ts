66e9f9ce30f3799db23c73af1cdd56b5867a4e79
izzatullohdev <izzatbekmadaminov7141@gmail.com>
2025-04-22 18:21:18 +0500

    validate almashtirldi

--- src/validation/testResult.schema.ts
+++ src/validation/testResult.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const testResultSchema = z.object({
   body: z.object({
-    wpm: z.number().min(1, "WPM kerak").max(200),
+    wpm: z.number().min(1, "WPM kerak").max(20000),
     raw: z.number().min(1, "Raw kerak"),
     consistency: z.number().min(0).max(100),
     time: z.number().min(1, "Vaqt noto'g'ri"),
