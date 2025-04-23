d08d68b75e281487457640b64a3491f4f90d6e4a
izzatullohdev <izzatbekmadaminov7141@gmail.com>
2025-04-23 12:12:50 +0500

    userni broz yangiladik

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,6 +6,20 @@ import { CustomError } from "../utils/customError";
 export const register = async (req: Request, res: Response) => {
   const { name, phone } = req.body;
   if (!name || !phone) throw new CustomError(404, "User topilmadi");
-  const user = await User.create({ name, phone });
-  res.status(200).json({ user });
+  const existingUser = await User.findOne({ where: { phone } });
+
+  if (existingUser) {
+    return res.status(200).json({
+      success: false,
+      message: "Siz allaqachon ro‘yxatdan o‘tgansiz",
+      user: existingUser,
+    });
+  }
+  const newUser = await User.create({ name, phone });
+
+  res.status(201).json({
+    success: true,
+    message: "Foydalanuvchi ro‘yxatdan o‘tdi",
+    user: newUser,
+  });
 };
